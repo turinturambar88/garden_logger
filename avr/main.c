79ef0a784bb5f7fd63f259696a0c01fd9df73b9a
Zach <zjonesengineering@gmail.com>
2016-10-29 21:36:04 -0700

    Makefile from AVR Programming book and stripped down main

---
+++ avr/main.c
@@ -0,0 +1,32 @@
+
+// Standard AVR includes
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+#include <avr/eeprom.h>
+#include <avr/pgmspace.h>
+#include <avr/sleep.h>
+#include <avr/wdt.h>
+#include <avr/power.h>
+
+// Standard includes
+#include <stdlib.h>
+
+
+// -------- Global Variables --------- //
+
+
+int main(void) 
+{
+    // -------- Inits --------- //
+    
+
+    // ------ Event loop ------ //
+    while (1)
+    {
+
+
+
+    }
+    return 0;
+}
